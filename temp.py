7b50c9be444a532472eb59535980c1425b1c815e
ritikakathare <ritikakathare15@gmail.com>
2026-01-15 15:35:23 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -3,7 +3,7 @@ if len(sys.argv) ==2:
     celsius=float(sys.argv[1])
 else:
     celsius=25
-    fahrenheit=(celsius*9/5)+32
+fahrenheit=(celsius*9/5)+32
 print("----------Temperature details------------")
 print("Celsius:",celsius)
 print("Fahrenheit:",fahrenheit)
